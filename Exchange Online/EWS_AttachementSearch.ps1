8043a56af79d2cbb1e1256f03ba85e4bf87bc16c
hibbertda <daniel.hibbert@microsoft.com>
2017-07-11 12:29:48 -0400

    updates

--- Exchange Online/EWS_AttachementSearch.ps1
+++ Exchange Online/EWS_AttachementSearch.ps1
@@ -1,17 +1,30 @@
-﻿$ServiceAccount = "EWSimp@hibblabs.org" ## Need Mailbox ##
+﻿#region GlobalVariables
+# Service account to connect to the service
+$ServiceAccount = "EWSimp@hibblabs.org" ## Need Mailbox ##
+
+# Path to EWS managed API
+$EWS_DLLPath = "C:\Program Files\Microsoft\Exchange\Web Services\2.2\Microsoft.Exchange.WebServices.dll"
 
 # Get service account credentials
 $credentials = Get-Credential
+#endregion
 
+#region ConnectToExchangeOnline
 # Connect to Exchange
-$EXO_Session = New-PSSession -ConfigurationName Microsoft.Exchange `
-    -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
-    -Credential ($credentials) `
-    -Authentication Basic `
-    -AllowRedirection `
-    -ErrorAction Stop
+<#
+    A connection to Exchange Online is needed to get a list of all user
+    mailboxes to search.
+#>
+#$EXO_Session = New-PSSession -ConfigurationName Microsoft.Exchange `
+#    -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
+#    -Credential ($credentials) `
+#    -Authentication Basic `
+#    -AllowRedirection `
+#    -ErrorAction Stop
 
-Import-PSSession $EXO_Session -ErrorAction Stop | Out-Null
+#Import-PSSession $EXO_Session -ErrorAction Stop | Out-Null
+
+#endregion
 
 # Set impresonation
 # MSDN: how to configure impersonation
@@ -23,8 +36,11 @@ Import-PSSession $EXO_Session -ErrorAction Stop | Out-Null
 $Mailboxes = Get-mailbox | ? {$_.RecipientTypeDetails -ne "DiscoveryMailbox"}
 
 # Import EWS managed API
-$EWS_DLLPath = "C:\Program Files\Microsoft\Exchange\Web Services\2.2\Microsoft.Exchange.WebServices.dll"
-Import-Module $EWS_DLLPath
+if ((test-path -Path $EWS_DLLPath) -eq $False){
+    Write-host -ForegroundColor red "Unable to find EWS Managed API 2.2"
+    exit
+}
+else {Import-Module $EWS_DLLPath}
 
 # Create connetction to EWS
 $creds = New-Object System.Net.NetworkCredential($credentials.UserName.ToString(),$credentials.GetNetworkCredential().password.ToString())
@@ -35,7 +51,7 @@ $Service.Credentials = $creds
 $service.AutodiscoverUrl($credentials.UserName.ToString(), {$true})
 
 
-$Mailboxes[0] | Foreach {
+$Mailboxes | Foreach-object {
 
     $service.ImpersonatedUserId = `
         New-Object Microsoft.Exchange.WebServices.Data.ImpersonatedUserId([Microsoft.Exchange.WebServices.Data.ConnectingIdType]::SmtpAddress,$_.PrimarySmtpAddress );
@@ -61,6 +77,29 @@ $Mailboxes[0] | Foreach {
     $ivItemView = New-Object Microsoft.Exchange.WebServices.Data.ItemView -ArgumentList 10 #(100)
     #$findItemsResults = $Inbox.FindItems($AttachmentTrueQuery,$ivItemView)
     $findItemsResults = $Inbox.FindItems($SearchCollection,$ivItemView)
+
+    $RunResult = @()
+
+    foreach($miMailItems in $findItemsResults.Items){
+        $miMailItems.Load()
+        foreach($attach in $miMailItems.Attachments){
+            $attach.Load()   
+
+            $obj = New-Object psobject -Property @{
+                Senders = $miMailItems.Sender
+                AttachmentName = $attach.name.tostring()
+                Received = $miMailItems.DateTimeReceived
+                "AttachmentSize(kb)" = [math]::round(($attach.Size / 1024),2)
+                Type = $attach.ContentType
+                Recivedby = $miMailItems.ReceivedBy
+
+            }
+
+            $RunResult += $obj
+        }
+    }
+  
+    $RunResult
 
     Write-Host -ForegroundColor white "Attachements recevind in last 24 hours: $($findItemsResults.TotalCount)"
 
